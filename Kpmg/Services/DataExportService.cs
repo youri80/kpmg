7e90711c32c95e0d175a785368479c7d2b3fa563
Christian Lange <christian@youri.de>
2018-11-18 08:05:06 +0100

    Init

---
+++ Kpmg/Services/DataExportService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace Kpmg.Services
+{
+    public class DataExportService
+    {
+        public DataExportService()
+        {
+        }
+    }
+}
---
+++ Kpmg/Services/MultiifondsDataService.cs
@@ -0,0 +1,29 @@
+﻿
+using System;
+using System.Data;
+using Oracle.ManagedDataAccess.Client;
+
+namespace Kpmg.Services
+{
+    public class DataLoadService
+    {
+        OracleConnection _con;
+
+        public DataLoadService(OracleConnection con)
+        {
+            _con = con;
+        }
+
+        public DataSet Load(string mfid, DateTime navdate){
+
+            var ds = new DataSet();
+            using (var com = new OracleCommand("",_con)) {
+
+            }
+        }
+
+        private string[] getMultifondsCls(string mfid){
+
+        }
+    }
+}
